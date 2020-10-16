9fd0adf8f9d403ab655e232eb0cb9cdd89cc7992
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-10-16 14:09:08 +0800

    [ParkingBoy][Vea] test - test_when_parking_boy_parks_car_into_parking_lot_and_returns_parking_ticket

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,4 +1,11 @@
 package com.oocl.cultivation;
 
 public class ParkingBoy {
+    public ParkingBoy(ParkingLot parkingLot) {
+
+    }
+
+    public ParkingTicket parkCar(Car car) {
+        return null;
+    }
 }
--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -1,10 +1,30 @@
 package com.oocl.cultivation;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ParkingBoyTest {
-    @Test
-    void write_your_first_test() {
+import static org.junit.jupiter.api.Assertions.assertNull;
 
+class ParkingBoyTest {
+    private Car car;
+    private ParkingLot parkingLot;
+    private ParkingBoy parkingBoy;
+    private ParkingTicket parkingTicket;
+
+    @BeforeEach
+    void setup(){
+        car = new Car();
+        parkingLot = new ParkingLot();
+        parkingTicket = new ParkingTicket();
+    }
+
+    @Test
+    void test_when_parking_boy_parks_car_into_parking_lot_and_returns_parking_ticket() {
+        //given
+         parkingBoy = new ParkingBoy(new ParkingLot());
+        //when
+        parkingTicket = parkingBoy.parkCar(car);
+        //then
+        assertNull(parkingTicket);
     }
 }
