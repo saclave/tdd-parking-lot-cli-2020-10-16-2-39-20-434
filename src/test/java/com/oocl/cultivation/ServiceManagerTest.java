2a0c8b4e3fc6d34fdaed8b227d0c668301393fc8
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-10-18 00:26:13 +0800

    [ParkingBoy][Vea] test - test_to_specify_which_parking_boy_can_park_car_by_service_manager

--- src/test/java/com/oocl/cultivation/ServiceManagerTest.java
+++ src/test/java/com/oocl/cultivation/ServiceManagerTest.java
@@ -1,24 +1,52 @@
 package com.oocl.cultivation;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ServiceManagerTest {
+    private ParkingBoy parkingBoy1 = new ParkingBoy();
+    private ParkingBoy parkingBoy2 = new ParkingBoy();
+    private ParkingBoy parkingBoy3 = new ParkingBoy();
+    private ParkingLot parkingLot1;
+    private ParkingLot parkingLot2;
+    private ParkingLot parkingLot3;
+    private Car car = new Car();
+    private ServiceManager serviceManager = new ServiceManager();
+
+    @BeforeEach
+    void setup(){
+    }
 
     @Test
     void test_service_manager_adding_parking_boys_to_the_management_list(){
         //given
-        ServiceManager serviceManager = new ServiceManager();
         ArrayList<ParkingBoy> parkingBoyArrayList = new ArrayList<>(asList
-                (new ParkingBoy(), new ParkingBoy(), new ParkingBoy()));
+                (parkingBoy1, parkingBoy2, parkingBoy3));
         //when
         serviceManager.setManagementList(parkingBoyArrayList);
         //then
         assertNotNull(serviceManager.manageParkingBoys());
+    }
+
+    @Test
+    void test_to_specify_which_parking_boy_can_park_car_by_service_manager(){
+        //given
+        parkingLot1 = new ParkingLot(1,0);
+        parkingLot2 = new ParkingLot(2, 0);
+        parkingLot3 = new ParkingLot(3, 0);
+        ArrayList<ParkingBoy> parkingBoyArrayList = new ArrayList<>(asList
+                (parkingBoy1, parkingBoy2, parkingBoy3));
+
+        parkingBoy1.setMultipleParkingLots(new ArrayList<>(asList(parkingLot1, parkingLot2, parkingLot3)));
+        serviceManager.setManagementList(parkingBoyArrayList));
+
+        assertNotNull(serviceManager.assignParkingBoy(1, car, parkingBoy1));
     }
 }
 
