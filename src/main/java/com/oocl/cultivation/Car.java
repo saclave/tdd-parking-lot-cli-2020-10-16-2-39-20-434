678182dbe3b8090a24b2311f20aa13042d928621
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-10-19 09:43:56 +0800

    [ParkingBoy][Vea] refactor - remove unused methods

--- src/main/java/com/oocl/cultivation/Car.java
+++ src/main/java/com/oocl/cultivation/Car.java
@@ -1,15 +1,5 @@
 package com.oocl.cultivation;
 
 public class Car {
-    private boolean isParked;
-
     public Car(){ }
-
-    public boolean isParked(){
-        return isParked;
-    }
-
-    public void setParked(boolean parked){
-        isParked = parked;
-    }
 }
